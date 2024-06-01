09e92ef5313b29f3d44a71d41dd07a6881da3940
Sergey Yaskov <yaskovdev@gmail.com>
2024-06-01 19:09:02 +0300

    Detect centers of the eyes.

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,15 @@
 import sys
 
 import cv2
+import numpy as np
+
+
+def _eyes_centers(landmark):
+    centers = []
+    for start in [36, 42]:
+        eye = landmark[0][start:start + 6]
+        centers.append(np.take(eye, [1, 2, 4, 5], axis=0).mean(axis=0))
+    return centers
 
 
 def preprocess(image_path):
@@ -17,8 +26,11 @@ def preprocess(image_path):
     landmark_detector.loadModel(LBFmodel)
     _, landmarks = landmark_detector.fit(image_gray, faces)
     for landmark in landmarks:
+        eyes_centers = _eyes_centers(landmark)
         for x, y in landmark[0]:
             cv2.circle(image, (int(x), int(y)), 1, (255, 255, 255), 1)
+        for x, y in eyes_centers:
+            cv2.circle(image, (int(x), int(y)), 1, (0, 0, 255), 1)
     cv2.imwrite("output.png", image)
 
 
